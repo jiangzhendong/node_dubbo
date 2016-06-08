3f5bf16f8f5969b0682c88581ec7321f235319e5
hacker <admin@admindeMacBook-Pro.local>
2016-06-08 17:31:31 +0800

    update

--- node_dubbo_server/target/node_dubbo_server/index.html
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>dubbo server start </h2>
-</body>
-</html>
--- node_front/routes/login.js
+++ node_front/routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Home Cinema' });
+  res.render('login', { title: 'Signin Template for My home' });
 });
 
 module.exports = router;
